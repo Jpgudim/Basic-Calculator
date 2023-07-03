0308e357c59e04c2c81a110d9ab9a3e67c321065
Jordan Gudim <137233322+Jpgudim@users.noreply.github.com>
2023-07-02 20:38:15 -0500

    Add files via upload
    - reworked the whole code. It now assigns each character from the input into a respective value in a dictionary. This allows multiple numbers and operators to be used in calculations. For example, previous version would only accept "10+10" but now current version will accept "10+10+10"
    
    - However, order of operations is not programmed yet so some calculations will not be correct.

--- Basic Calculator.py
+++ Basic Calculator.py
@@ -2,35 +2,49 @@ print ("Welcome to the calculator! What do you want to calculate?")
 input = input("")
 input_2 = input.replace(" ","")
 
-num1 = ""
-num2 = ""
-  
+#defining dictionaries and placeholder variables (n and m)
+
+numbers = {1:"", 2:"", 3:""}
+n = 1
+m = 1
+operat = {1:"", 2:"", 3:""}
+ans2 = 0
+
+#loop to turn intput into usable variables in dictionaries
+
 for char in input_2:
     if char in "0123456789":
-        num1 += char
-    else:
-        break
+        numbers[n] += char
+    if char in "Xx*/+-":
+        n += 1
+        operat[m] = char
+        m += 1
 
-length = len(num1) + 1
-num2 = input_2[int(length):]
+#this code words, but order of operations does not exist
+#for example, 10+10x20 will output 400 when it should output 210
 
-num1 = int(num1)
-num2 = int(num2)
+if operat[1] == "X" or operat[1] == "x" or operat[1] == "*":
+    ans1 = int(numbers[1]) * int(numbers[2])
+if operat[1] == "/":
+    ans1 = int(numbers[1]) / int(numbers[2])
+if operat[1] == "+":
+    ans1 = int(numbers[1]) + int(numbers[2])
+if operat[1] == "-":
+    ans1 = int(numbers[1]) - int(numbers[2])
 
-print()
-if "X" in input_2 or "x" in input_2 or "*" in input_2:
-    answer = num1 * num2
-    print (input + " = " + str(answer))
-elif "/" in input_2:
-    answer = num1 / num2
-    print (input + " = " + str(answer))
-elif "+" in input_2: 
-    answer = num1 + num2
-    print (input + " = " + str(answer))
-elif "-" in input_2:
-    answer = num1 - num2
-    print (input + " = " + str(answer))
-print ()
+if operat[2] == "X" or operat[2] == "x" or operat[2] == "*":
+    ans2 = ans1 * int(numbers[3])
+if operat[2] == "/":
+    ans2 = ans1 / int(numbers[3])
+if operat[2] == "+":
+    ans2 = ans1 + int(numbers[3])
+if operat[2] == "-":
+    ans2 = ans1 = int(numbers[3])
 
-
+if ans2 != 0:
+    print (input + " = " + str(ans2))
+    print()
+else:
+    print (input + " = " + str(ans1))
+    print()
 
