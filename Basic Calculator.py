7c5ded6ca55354a639214b19e38fd4a2a85ca203
Jordan Gudim <137233322+Jpgudim@users.noreply.github.com>
2023-07-09 20:32:02 -0500

    Added order of operations
    patch notes:
    - added order of operations successfully!
    
    - calculator can now have an unlimited amount of numbers/operations because dictionaries are now able to grow
    
    issues:
    - need to put in checks for if the user enters invalid input, like letters or a double operator (ex: */)
    
    - would like to print out the input with the answer instead of just the answer(ex: 10 + 10 = 4)

--- Basic Calculator.py
+++ Basic Calculator.py
@@ -4,12 +4,14 @@ input_2 = input.replace(" ","")
 
 #defining dictionaries and placeholder variables (n and m)
 
-numbers = {1:"", 2:"", 3:""}
+numbers = {1:""}
 n = 1
 m = 1
-operat = {1:"", 2:"", 3:""}
+
+
+operat = {}
 ans2 = 0
-answer = {1:"", 2:"", 3:""}
+answer = {}
 final = 0
 
 #loop to turn intput into usable variables in dictionaries
@@ -19,69 +21,46 @@ for char in input_2:
         numbers[n] += char
     if char in "Xx*/+-":
         n += 1
+        numbers[n] = ""
+        operat[m] = ""
         operat[m] = char
         m += 1
-
-# this code below is the plan for coding order of operations.
-# making progress, still need to figure out how to not add previous numbers together. IE 10+10+10 is outputting 40 because it is doing 10+10 twice, rather than doing 20+10
-# not sure how to solve this, perhaps deleting numbers from dictionary once they are used in calculation?
-
 
 for key in operat:
     if operat[key] == "X" or operat[key] == "x":
         if key == 1:
             answer[key] = int(numbers[key]) * int(numbers[key + 1])
+            numbers[key] = answer[key]
         else:
             answer[key] = int(numbers[key - 1]) * int(numbers[key + 1])
+            numbers[key] = answer[key]
     if operat[key] == "/":
         if key == 1:
             answer[key] = int(numbers[key]) / int(numbers[key + 1])
+            numbers[key] = answer[key]
         else:
             answer[key] = int(numbers[key - 1]) / int(numbers[key + 1])
+            numbers[key] = answer[key]
     if operat[key] == "+":
         if key == 1:
             answer[key] = int(numbers[key]) + int(numbers[key + 1])
+            numbers[key] = answer[key]
         else:
             answer[key] = int(numbers[key - 1]) + int(numbers[key + 1])
+            numbers[key] = answer[key]
     if operat[key] == "-":
         if key == 1:
             answer[key] = int(numbers[key]) - int(numbers[key + 1])
+            numbers[key] = answer[key]
         else:
             answer[key] = int(numbers[key - 1]) - int(numbers[key + 1])
+            numbers[key] = answer[key]
 
-
+final_answer = answer.copy()
 
 for key in answer:
-    if  answer[key] != "":
-        final += int(answer[key])
-
-print (int(final))
-
-#this below code works, but order of operations does not exist
-#for example, 10+10x20 will output 400 when it should output 210
-
-#if operat[1] == "X" or operat[1] == "x" or operat[1] == "*":
-    #ans1 = int(numbers[1]) * int(numbers[2])
-#if operat[1] == "/":
-    #ans1 = int(numbers[1]) / int(numbers[2])
-#if operat[1] == "+":
-    #ans1 = int(numbers[1]) + int(numbers[2])
-#if operat[1] == "-":
-    #ans1 = int(numbers[1]) - int(numbers[2])
-#if operat[2] == "X" or operat[2] == "x" or operat[2] == "*":
-    #ans2 = ans1 * int(numbers[3])
-#if operat[2] == "/":
-    #ans2 = ans1 / int(numbers[3])
-#if operat[2] == "+":
-    #ans2 = ans1 + int(numbers[3])
-#if operat[2] == "-":
-    #ans2 = ans1 = int(numbers[3])
-
-#if ans2 != 0:
-    #print (input + " = " + str(ans2))
-    #print()
-#else:
-    #print (input + " = " + str(ans1))
-    #print()
+    if  answer[key] == "":
+        del final_answer[key]
+print (final_answer[list(final_answer.keys())[-1]])
 
 
