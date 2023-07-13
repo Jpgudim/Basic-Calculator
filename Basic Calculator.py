827c0b528bc069335fbd90ac8fcf20a6f8df3207
Jordan Gudim <137233322+Jpgudim@users.noreply.github.com>
2023-07-13 16:54:32 -0500

    Project complete (for now)
    patch notes:
    - added a function that receives and checks the user input for letters and touching operators. Uses a while loop to allow the user to keep inputting a string until a valid string is received
    
    - removed code to remove spaces from user input for calculations. The for loop that created the numbers dictionary would skip over spaces anyway, so removing the spaces was unnecessary.
    
    issues:
    - none at the moment

--- Basic Calculator.py
+++ Basic Calculator.py
@@ -1,6 +1,20 @@
 print ("Welcome to the calculator! What do you want to calculate?")
-input = input("")
-input_2 = input.replace(" ","")
+
+#function to receive and check user input
+def get_input():
+    userinput = "invalid"
+    while userinput == "invalid":
+        userinput = input("")
+        if userinput.isupper() == True or userinput.islower() == True:
+            print ("That is an invalid input. Try again:")
+            userinput = "invalid"
+        for n in range(0,len(userinput)):
+            if userinput[n] in "Xx*/+-" and userinput[n+1] in "Xx*/+-":
+                print ("That is an invalid input. Try again:")
+                userinput = "invalid"
+    return userinput
+
+userinput = get_input()
 
 #defining dictionaries and placeholder variables (n and m)
 numbers = {1:""}
@@ -8,7 +22,7 @@ n = 1
 m = 1
 
 #loop to turn intput into usable variables in dictionaries
-for char in input_2:
+for char in userinput:
     if char in "0123456789":
         numbers[n] += char
     if char in "Xx*/+-":
@@ -17,7 +31,7 @@ for char in input_2:
         numbers[n] = char
         n += 1
         numbers [n] = ""
-    
+
 #first for loop to calculate multiplication and division first
 for key in numbers:
     z = 1
@@ -58,9 +72,10 @@ for key in numbers:
         numbers[key] = int(numbers[key - o]) - int(numbers[key + z])
         numbers[key - o] = ""
         numbers[key + z] = ""
+
 #for loop that iterates through the dictionary and prints out the only remaining number, which is the final answer
 for key in numbers:
     if numbers[key] != "":
-        print (str(input) + " = " + str(numbers[key]))
+        print (str(userinput) + " = " + str(numbers[key]))
 
 
