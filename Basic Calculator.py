eaa43df2014ae406824c90f212552684932a7564
Jordan Gudim <137233322+Jpgudim@users.noreply.github.com>
2023-07-12 23:39:04 -0500

    ORDER OF OPERATIONS WORKS!
    patch notes:
    - order of operations finally works! The program now uses one dictionary rather than separate dictionaries for numbers and operators. This allows the program to properly do calculations with multiple operators in the correct order of operations. Basically, numbers and operators that are used in calculations are replaced by an empty space. The loops then iterate through the dictionary to find the next number that is not an empty space and uses that number in the next calculation.
    
    For example, let's say the user inputs 1+1+1
    - the program creates a dictionary and adds each character: 1, +, 1, +, 1
    - it does the first calculation (1+1) and replaces values "+" and "1" since they were used in the calculation
    - the dictionary now looks like this: 2, _, _, +, 1
    - the dictionary then finds the next value that is not a blank space (in this case, 2)
    - then the program does 2 + 1
    - since "+" and "1" are used in a calculation, the program removes them from the dictionary and replaces the 2 with the new sum, 3.
    - the dictionary now looks like 3, _, _, _, _,
    - the program now outputs 3 as the final answer.
    
    I basically had to redo most of the code. I also should probably create a "read me" file explaining that top part better.
    
    issues:
    - would like to add checks to make sure the user is inputting a proper string. So basically making sure there are no letters or incorrect formatting like double operators (ex: +- or *+)

--- Basic Calculator.py
+++ Basic Calculator.py
@@ -6,10 +6,6 @@ input_2 = input.replace(" ","")
 numbers = {1:""}
 n = 1
 m = 1
-operat = {}
-ans2 = 0
-answer = {}
-final = 0
 
 #loop to turn intput into usable variables in dictionaries
 for char in input_2:
@@ -18,54 +14,53 @@ for char in input_2:
     if char in "Xx*/+-":
         n += 1
         numbers[n] = ""
-        operat[m] = ""
-        operat[m] = char
-        m += 1  
-
+        numbers[n] = char
+        n += 1
+        numbers [n] = ""
+    
 #first for loop to calculate multiplication and division first
-for key in operat:
-    if operat[key] in "Xx*/":
-        if operat[key] == "X" or operat[key] == "x" or operat[key] == "*":
-            if key == 1:
-                    answer[key] = int(numbers[key]) * int(numbers[key + 1])
-                    numbers[key] = answer[key]
-            else:
-                answer[key] = int(numbers[key - 1]) * int(numbers[key + 1])
-                numbers[key] = answer[key]
-            operat[key] = ""
-        if operat[key] == "/":
-            if key == 1:
-                answer[key] = int(numbers[key]) / int(numbers[key + 1])
-                numbers[key] = answer[key]
-            else:
-                answer[key] = int(numbers[key - 1]) / int(numbers[key + 1])
-                numbers[key] = answer[key]
-            operat[key] = ""
-print (numbers)
-print (operat)
-print (answer)
-print()
+for key in numbers:
+    z = 1
+    o = 1
+    if numbers[key] == "X" or numbers[key] == "x" or numbers[key] == "*":
+        while str(numbers[key - o]) in "Xx*/+- ":
+            o += 1
+        while str(numbers[key + z]) in "Xx*/+- ":
+            z += 1
+        numbers[key] = int(numbers[key - o]) * int(numbers[key + z])
+        numbers[key - o] = ""
+        numbers[key + z] = ""
+    if numbers[key] == "/":
+        while str(numbers[key - o]) in "Xx*/+- ":
+            o += 1
+        while str(numbers[key + z]) in "Xx*/+- ":
+            z += 1
+        numbers[key] = int(numbers[key - o]) / int(numbers[key + z])
+        numbers[key - o] = ""
+        numbers[key + z] = ""
 #second for loop to calculate addition and subtraction
-for key in operat:
-    if operat[key] == "+":
-        if key == 1:
-            answer[key] = int(numbers[key]) + int(numbers[key + 1])
-            numbers[key] = answer[key]
-        else:
-            answer[key] = int(numbers[1]) + int(numbers[key + 1])
-            numbers[key] = answer[key]
-    if operat[key] == "-":
-        if key == 1:
-            answer[key] = int(numbers[key]) - int(numbers[key + 1])
-            numbers[key] = answer[key]
-        else:
-            answer[key] = int(numbers[1]) - int(numbers[key + 1])
-            numbers[key] = answer[key]
-
-print (numbers)
-print (operat)
-print (answer)
-
-print (str(input) + " = " + str(answer[list(answer.keys())[-1]]))
+for key in numbers:
+    z = 1
+    o = 1
+    if numbers[key] == "+":
+        while str(numbers[key - o]) in "Xx*/+- ":
+            o += 1
+        while str(numbers[key + z]) in "Xx*/+- ":
+            z += 1
+        numbers[key] = int(numbers[key - o]) + int(numbers[key + z])
+        numbers[key - o] = ""
+        numbers[key + z] = ""
+    if numbers[key] == "-":
+        while str(numbers[key - o]) in "Xx*/+- ":
+            o += 1
+        while str(numbers[key + z]) in "Xx*/+- ":
+            z += 1
+        numbers[key] = int(numbers[key - o]) - int(numbers[key + z])
+        numbers[key - o] = ""
+        numbers[key + z] = ""
+#for loop that iterates through the dictionary and prints out the only remaining number, which is the final answer
+for key in numbers:
+    if numbers[key] != "":
+        print (str(input) + " = " + str(numbers[key]))
 
 
